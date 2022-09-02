5757d72f71a5c5532fd29bc8489f5edb3cf6f8d9
toco <ttachibana@my.bcit.ca>
2022-09-02 15:20:29 -0700

    do while loops

---
+++ dowhile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    int sum = 0;
+
+    do {
+        printf("Enter a # above 0: ");
+        scanf("%d", &number);
+
+        if (number > 0)
+        {
+            sum += number;
+        }
+    } while (number > 0);
+
+    printf("The sum is: %d ", sum);
+
+    return 0;
+}
