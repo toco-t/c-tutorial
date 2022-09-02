a98c0532cfdb1b4ff0769e36de25c6dda809fbf5
toco <ttachibana@my.bcit.ca>
2022-09-02 15:20:02 -0700

    string functions

---
+++ stringfunctions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char owner[] = "Toco";
+    char cat[] = "Alpha";
+
+    // strcat, strncat, strcpy, strncpy
+    // strlen, strcmp, strncmp,
+
+    printf("%s and %s", owner, cat);
+
+    return 0;
+}
